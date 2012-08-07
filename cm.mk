287a392945c23084021cc753376e6abace5eaae5
Jeffrey Clark <dude@zaplabs.com>
2012-08-07 02:51:58 -0500

    quincyatt: jellybean fingerprint
    Change-Id: If1597b134d92f759d0a006f0f671f013467c0aae

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.1/IMM76D/UCLF6:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.1 IMM76D UCLF6 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.1/JRO03H/UCLF6:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.1 JRO03H UCLF6 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-800x1280
 
