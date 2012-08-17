c7461f781a4141c1c48e7b683c1d91153af367a3
Jeffrey Clark <dude@zaplabs.com>
2012-08-17 13:33:18 -0500

    h0tsauce property overrides
    Change-Id: Ied456703f1e9e95e25e3254cb4cf52a9813aada3

--- full_quincyatt.mk
+++ full_quincyatt.mk
@@ -38,3 +38,22 @@ PRODUCT_DEVICE := quincyatt
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := SAMSUNG-SGH-I717
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=/system/lib/libqc-opt.so \
+    ro.qualcomm.cabl=0 \
+    ro.ril.set.mtu1472=1 \
+    persist.sys.usb.config=mass_storage \
+    dalvik.vm.checkjni=false \
+    dalvik.vm.dexopt-flags=m=y,v=n,o=v \
+    ro.telephony.sends_barcount=true \
+    ro.cm.version=h0tsauce-10-$(shell date -u +%Y%m%d) \
+    ro.modversion=$(shell date -u +%Y%m%d)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rommanager.developerid=h0tw1r3 \
+    ro.developerid=h0tw1r3 \
+    ro.goo.developerid=h0tw1r3 \
+    ro.goo.rom=h0tsauce-10 \
+    ro.goo.version=$(shell date -u +%Y%m%d) \
+    ro.goo.board=SGH-I717
